427dbe593dbcc6c3d2d510be7334b1bdcf7486d7
MC-Teacher <mc.teach.21@gmail.com>
2021-07-02 11:48:43 +0200

    Initial commit

---
+++ app/src/main/java/mc/apps/start/StartActivity.java
@@ -0,0 +1,74 @@
+package mc.apps.start;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.BounceInterpolator;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.transition.ChangeBounds;
+import androidx.transition.Transition;
+import androidx.transition.TransitionListenerAdapter;
+import androidx.transition.TransitionManager;
+
+public class StartActivity extends AppCompatActivity {
+    ImageView logo;
+    ConstraintLayout root;
+    //val TAG = "network"
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_begin);
+
+        logo = findViewById(R.id.logo);
+        root = findViewById(R.id.root);
+
+        getSupportActionBar().hide();
+        startAnimation();
+    }
+    private void startAnimation() {
+        AnimatorSet animatorSet = new AnimatorSet();
+        ValueAnimator fadeAnim = ObjectAnimator.ofFloat(logo, "alpha", 0f, 1f);
+        fadeAnim.setDuration(500);
+        fadeAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                animateLogo();
+            }
+        });
+
+        animatorSet.play(fadeAnim);
+        animatorSet.start();
+    }
+
+    private void animateLogo() {
+        ConstraintSet finishingConstraintSet = new ConstraintSet();
+        finishingConstraintSet.clone(this, R.layout.activity_start);
+        Transition transition = new ChangeBounds();
+        transition.setDuration(1000);
+        transition.setInterpolator(new BounceInterpolator());
+
+        transition.addListener(new TransitionListenerAdapter() {
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                open();
+            }
+        });
+
+        TransitionManager.beginDelayedTransition(root, transition);
+        finishingConstraintSet.applyTo(root);
+    }
+
+    private void open() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
